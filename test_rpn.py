4aa8cf2591cedf49338b4ae4f45dadf70a7e8001
David Spehar <spehar@umich.edu>
2017-03-13 14:44:49 -0400

    Update test_rpn.py

--- test_rpn.py
+++ test_rpn.py
@@ -8,7 +8,7 @@ class TestBasics(unittest.TestCase):
 	def test_subtract(self):
 		result = rpn.calculate('5 3 -')
 		self.assertEqual(2, result)
-	def test_subtract(self):
+	def test_multiply(self):
 		result = rpn.calculate('5 3 *')
 		self.assertEqual(15, result)
 	def test_exponentiation(self):
